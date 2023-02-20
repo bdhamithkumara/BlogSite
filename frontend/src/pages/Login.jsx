a64899de7993b88214a43d57ddbe2038e5d8a14c
Damithkumara <damithkumararoxx55@gmail.com>
2023-02-20 11:29:30 +0530

    designed the login page ✈

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className='auth flex  flex-col items-center justify-center h-[100vh] bg-[#A555EC]'>
+      <h1 className='text-[30px] uppercase text-[#ffffff] mb-[20px]'>Login</h1>
+      <form className='flex - flex-col p-[50px] bg-[#ffff] rounded-xl w-[350px] gap-[20px]'>
+        <input className='inputf' type="text" placeholder='username' />
+        <input className='inputf' type="password" placeholder='password' />
+        <button className='p-[10px] border-0 bg-[#A555EC] cursor-pointer text-white'>Login</button>
+        <p className='text-[12px] text-red-700 text-center'>This is an error!</p>
+        <span className='text-[12px] text-center'>Don't you have an account? <Link to="/register">Register</Link></span>
+      </form>
+    </div>
+  )
+}
+
+ 
+
+export default Login
