90a52e7c1941a5e36cc23537e760c9bdb13250ca
Damithkumara <damithkumararoxx55@gmail.com>
2023-02-20 11:58:17 +0530

    designed register page ⚓

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth flex  flex-col items-center justify-center h-[100vh] bg-[#A555EC]'>
+    <h1 className='text-[30px] uppercase text-[#ffffff] mb-[20px]'>register</h1>
+    <form className='flex - flex-col p-[50px] bg-[#ffff] rounded-xl w-[350px] gap-[20px]'>
+      <input required className='inputf' type="text" placeholder='username' />
+      <input required className='inputf' type="email" placeholder='email' />
+      <input required className='inputf' type="password" placeholder='password' />
+      <button className='p-[10px] border-0 bg-[#A555EC] cursor-pointer text-white'>Login</button>
+      <p className='text-[12px] text-red-700 text-center'>This is an error!</p>
+      <span className='text-[12px] text-center'>Do you have an account? <Link to="/login">Login</Link></span>
+    </form>
+  </div>
   )
 }
+
+
 
 export default Register
